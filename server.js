4d9b0a4534c00dc5a1e1ed954ed3994dbb8e500d
Kaylee Spindler <gl13ch@Kaylees-MacBook-Air.local>
2021-11-03 13:26:25 -0500

    updating for heroku

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-require('dotenv').config()
 
 const app = express()
+const db = mongoose.connection;
+
+require('dotenv').config()
 const PORT = process.env.PORT || 3003
 
 app.use(express.json())
@@ -11,6 +13,10 @@ app.use(cors())
 
 const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGODB_URI);
+
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
 app.get('/', (req, res) => {
   res.send('Hello')
