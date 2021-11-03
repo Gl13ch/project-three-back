23e0643b07588f6208e3eef7884c326f2bed1dae
Kaylee Spindler <gl13ch@Kaylees-MacBook-Air.local>
2021-11-03 12:47:18 -0500

    stuff to deploy

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3003
+
+app.use(express.json())
+app.use(cors())
+
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose.connect(MONGODB_URI);
 
 app.get('/', (req, res) => {
   res.send('Hello')
 })
 
 app.listen(PORT, () => {
-  console.log('Listening on...', PORT);;
+  console.log('Listening on...', PORT);
+  // console.log(process.env);
 })
+
+mongoose.connect('mongodb://localhost:27017/proj3')
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongod...');
+});
