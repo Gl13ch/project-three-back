404b759d5f0715504a10e004c81283413a8c7163
Kaylee Spindler <gl13ch@Kaylees-MacBook-Air.local>
2021-11-03 14:47:29 -0500

    made it a bit more readable

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-
 const app = express()
 const db = mongoose.connection;
-
 require('dotenv').config()
+
+//Port
 const PORT = process.env.PORT || 3003
 
-app.use(express.json())
-app.use(cors())
-
+//Database
 const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGODB_URI);
 
+//error messages
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 db.on('disconnected', () => console.log('mongo disconnected'));
+
+//middleware
+app.use(express.json())
+app.use(cors())
 
 //controllers
 const storeController = require('./controllers/store.js');
 app.use('/store', storeController)
 
-
+//Listeners
 app.listen(PORT, () => {
   console.log('Listening on...', PORT);
-  // console.log(process.env);
 })
 
 mongoose.connect('mongodb://localhost:27017/proj3')
